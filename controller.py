3e0ec3de5d6d2be4e564dd91b63cd0c07882aab2
VicerExciser <acondict11@gmail.com>
2020-09-13 05:35:58 -0400

    syntax fixes & refactoring

--- controller.py
+++ controller.py
@@ -11,7 +11,7 @@ class TemperatureController():
 		self.ow_bus = OneWire.get_instance()
 		self.sensor_class = sensor_type
 		self.sensors = []
-		for sensor in self.ow_bus.search(self.sensor_class, ds18b20.rom_cmds['SRCH_ROM']):
+		for sensor in self.ow_bus.search(self.sensor_class, self.sensor_class.rom_cmds['SRCH_ROM']):
 			self.sensors.append(sensor)
 			print(f"[{self.__class__.__name__}]\tFound {self.sensor_class.__name__}:  {repr(sensor)}")
 
@@ -104,7 +104,7 @@ class TemperatureController():
 		fahr = ds18b20.fahr_from_celsius(cels)
 
 		''' Display Read registers '''
-		print(f'\nDEVICE {ichoice} TEMPERATURE = [{cels}{ds18b20.DEG_C} | {fahr}{ds18b20.DEG_F}] \n')
+		print(f'\nDEVICE {ichoice} TEMPERATURE = [{cels}°C | {fahr}°F] \n')
 		# target_id = OneWire.get_id(ichoice)
 
 		#sensor.last_read_temp = fahr
@@ -116,15 +116,15 @@ class TemperatureController():
 ## ---------------------------------------------------------------------------------------------
 
 	# def match_rom(ichoice):
-	def match_rom(rom_lo, rom_hi):
+	def match_rom(self, rom_lo, rom_hi):
 		"""
 		MATCH ROM [55h]
 		The match ROM command allows to address a specific slave device on a multidrop or single-drop bus.
 		Only the slave that exactly matches the 64-bit ROM code sequence will respond to the function command
 		issued by the master; all other slaves on the bus will wait for a reset pulse.
 		"""
-		OneWire.bram_write(addr_map['CMD_ADDR'], ds18b20.rom_cmds['MTCH_ROM'])
-		OneWire.bram_write(addr_map['WRS_ADDR'], ds18b20.TRANSMIT_BITS)
+		OneWire.bram_write(addr_map['CMD_ADDR'], self.sensor_class.rom_cmds['MTCH_ROM'])
+		OneWire.bram_write(addr_map['WRS_ADDR'], self.sensor_class.TRANSMIT_BITS)
 		# OneWire.bram_write(addr_map['WR0_ADDR'], OneWire.rom_addrs[ichoice * 2])
 		# OneWire.bram_write(addr_map['WR1_ADDR'], OneWire.rom_addrs[(ichoice * 2) + 1])
 		OneWire.bram_write(addr_map['WR0_ADDR'], rom_lo)
@@ -145,28 +145,28 @@ class TemperatureController():
 
 ## ---------------------------------------------------------------------------------------------
 
-	def convert_temp():
+	def convert_temp(self):
 		"""
 		CONVERT T [44h]
 		This command initiates a single temperature conversion.
 		"""
-		OneWire.bram_write(addr_map['CMD_ADDR'], ds18b20.func_cmds['CONVT_TEMP'])
+		OneWire.bram_write(addr_map['CMD_ADDR'], self.sensor_class.func_cmds['CONVT_TEMP'])
 		OneWire.bram_write(addr_map['CON_ADDR'], bus_cmds['EXEC_W_PULLUP'])
-		time.sleep(ds18b20.T_CONV)
+		time.sleep(self.sensor_class.T_CONV)
 		r_status = OneWire.bram_read(addr_map['STA_ADDR'])
 		return r_status == 0x8
 
 	
 ## ---------------------------------------------------------------------------------------------
 
-	def read_scratch():
+	def read_scratch(self):
 		"""
 		READ SCRATCHPAD [BEh]
 		This command allows the master to read the contents of the scratchpad register.
 		Note: master must generate read time slots immediately after issuing the command.
 		"""
-		OneWire.bram_write(addr_map['CMD_ADDR'], ds18b20.func_cmds['SCRATCH_RD'])
-		OneWire.bram_write(addr_map['RDS_ADDR'], ds18b20.SCRATCH_RD_SIZE)
+		OneWire.bram_write(addr_map['CMD_ADDR'], self.sensor_class.func_cmds['SCRATCH_RD'])
+		OneWire.bram_write(addr_map['RDS_ADDR'], self.sensor_class.SCRATCH_RD_SIZE)
 		OneWire.bram_write(addr_map['CON_ADDR'], bus_cmds['RD_TIME_SLOTS'])
 		r_status = OneWire.bram_read(addr_map['STA_ADDR'])
 		x = r_status & masks['STA_RDD']
